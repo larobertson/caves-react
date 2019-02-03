b49bf3b8b3ceaf0ffb83d944ad24409ff241b3d6
Laura <laura.robertson64@gmail.com>
2019-02-02 20:20:16 -0600

    app now a class rendering a header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>I'm so lonely without anything to complete</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+    <h1>Caves Around the World</h1>
+    )}
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
